735393a3abc2e8d074921e94810e080cf2f57c61
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2026-04-19 21:30:51 -0300

    feat: Criando classe de Mapeamento entidade Customer

---
+++ aub-backend/Application/Profiles/CustomerProfile.cs
@@ -0,0 +1,14 @@
+﻿using aub_backend.Application.DTOs;
+using aub_backend.Domain.Entities;
+using AutoMapper;
+
+namespace aub_backend.Application.Profiles
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, CustomerResponseDto>();
+        }
+    }
+}
